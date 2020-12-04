20b5ea3a2e559258806a2cb280e040e7edf68c89
morganbronson <morgan.bronson@ocsbstudent.ca>
2020-12-04 15:37:34 +0000

    put my script in the js

---
+++ js/script.js
@@ -0,0 +1,21 @@
+    "use strict";
+    alert( 'Hello, world!' );
+
+    // this comment takes up a line
+    alert(6 +
+    1
+    + 3);
+
+    let user = 'John';
+    let age = 25;
+    let message = 'Hello';
+    alert(message); // Hello World
+
+    const COLOR_RED = "#F00";
+    const COLOR_GREEN = "#0F0";
+    const COLOR_BLUE = "#00F";
+    const COLOR_ORANGE = "#FF7F00";
+    // ...when we need to pick a color
+    let color = COLOR_BLUE;
+    alert(color); // #FF7F00
+    
