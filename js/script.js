f7777fda700b417a96b4576aef5ef262a2311b0d
morganbronson <morgan.bronson@ocsbstudent.ca>
2020-12-04 16:04:09 +0000

    Increment & Decrement

--- js/script.js
+++ js/script.js
@@ -51,3 +51,29 @@
     let oranges = "3";
     alert(apples + oranges); // "23", the binary plus concatenates strings
 
+    let a = 1;
+    let b = 2;
+
+    let c = 3 - (a = b + 1);
+
+    alert( a ); // 3
+    alert( c ); // 0
+
+    c = 2 + 2;
+    b = c;
+    a = c;
+
+    let n = 2;
+    n += 5; // now n = 7 (same as n = n + 5)
+    n *= 2; // now n = 14 (same as n = n * 2)
+    alert( n ); // 14
+
+    let counter = 2;
+    counter++;        // works the same as counter = counter + 1, but is shorter
+    alert( counter ); // 3
+
+    let counter = 2;
+    counter--;        // works the same as counter = counter - 1, but is shorter
+    alert( counter ); // 1
+
+    
